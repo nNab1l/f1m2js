0eabc020346e19736d4c009f1a23422ecc00a1f3
Nabil <33580@ma-web.nl>
2021-11-29 21:03:22 +0100

    opdracht 2

---
+++ web/les 2 boodschappen/script.js
@@ -0,0 +1,23 @@
+console.log("script is geladen!");
+
+const groentevak = document.getElementById('groente');
+const fruitvak = document.getElementById('fruit');
+
+function zetInFruitLa(fruit){
+    console.log(fruit);
+
+    const nieuwElement = document.createElement('h2');
+    nieuwElement.innerHTML = fruit;
+    nieuwElement.className = 'rood';
+    fruitvak.appendChild(nieuwElement)
+}
+
+function zetInGroenteLa(groente){
+    console.log(groente);
+
+    const nieuwElement = document.createElement('h2');
+    nieuwElement.innerHTML = groente;
+    nieuwElement.className = 'groen';
+    groentevak.appendChild(nieuwElement)
+}
+
